7b4dfb0d1e4a7dcaf632956b6bd889bd7d90c441
vijayakumarSankaran <vijayakumar.s.m3@gmail.com>
2025-04-01 20:59:47 +0530

    new changes

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -1,25 +1,27 @@
 import { test, expect, Page } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPAge';
- import * as dotenv from 'dotenv';
+//import * as dotenv from 'dotenv';
 
- dotenv.config();
+ //dotenv.config();
 
 test('Valid Login Test', async ({ page }: { page: Page }) => {
-    const email = process.env.EMAIL;
-    const password = process.env.PASSWORD;
-    console.log('email is: ', email);
-    console.log('password is: ', password);
+    // const email = process.env.EMAIL;
+    // const password = process.env.PASSWORD;
+    // console.log('email is: ', email);
+    // console.log('password is: ', password);
 
-    if (!email || !password) {
-        throw new Error('Email or Password is not set in the .env file');
-    }
+    // if (!email || !password) {
+    //     throw new Error('Email or Password is not set in the .env file');
+    // }
 
     const loginPage = new LoginPage(page);
    
+    // WebDriver driver = new ChromeDriver();
+    // driver.get("https://tutorialsninja.com/demo/index.php?route=account/login")
 
     await page.goto('https://tutorialsninja.com/demo/index.php?route=account/login');
-   // await loginPage.login('Test', 'Test@123');
-   await loginPage.login(email,password);
+    await loginPage.login('Test', 'Test@123');
+   //await loginPage.login(email,password);
     await page.waitForTimeout(2000);
     console.log('Login successfully...');
     
@@ -30,6 +32,8 @@ test('Valid Login Test', async ({ page }: { page: Page }) => {
 //     await expect(page).toHaveURL('https://tutorialsninja.com/demo/index.php?route=account/account');
 //    await expect(page).toHaveTitle('My Account');
 //   expect(await loginPage.isLoginButtonVisible()).toBe(false); 
+
+
 });
 
 
